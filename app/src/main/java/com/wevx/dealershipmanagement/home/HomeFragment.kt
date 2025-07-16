e2e10fa54f0f6212c122657a129dbcb36252b757
Rifat Hossain <mdrifathossain472@gmail.com>
2025-07-17 03:10:22 +0600

    initial commit

---
+++ app/src/main/java/com/wevx/dealershipmanagement/home/HomeFragment.kt
@@ -0,0 +1,15 @@
+package com.wevx.dealershipmanagement.home
+
+import com.wevx.dealershipmanagement.base.BaseFragment
+import com.wevx.dealershipmanagement.databinding.FragmentHomeBinding
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
+    override fun setAllClickListener() {
+
+    }
+
+    override fun allObserver() {
+
+    }
+
+}
