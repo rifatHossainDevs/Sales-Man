1efcffcf079ab3d9f5a0ba08114d3786394285a0
Rifat Hossain <mdrifathossain472@gmail.com>
2025-07-21 00:21:50 +0600

    initial commit

---
+++ app/src/main/java/com/wevx/dealershipmanagement/payment/PaymentFragment.kt
@@ -0,0 +1,21 @@
+package com.wevx.dealershipmanagement.payment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.wevx.dealershipmanagement.R
+import com.wevx.dealershipmanagement.base.BaseFragment
+import com.wevx.dealershipmanagement.databinding.FragmentPaymentBinding
+
+class PaymentFragment : BaseFragment<FragmentPaymentBinding>(FragmentPaymentBinding::inflate) {
+    override fun setAllClickListener() {
+
+    }
+
+    override fun allObserver() {
+
+    }
+
+}
