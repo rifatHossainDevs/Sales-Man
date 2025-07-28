39f57c2e366a956de7ae946b17f848f37fd917ad
Rifat Hossain <mdrifathossain472@gmail.com>
2025-07-29 03:10:33 +0600

    initial commit

--- app/src/main/java/com/wevx/dealershipmanagement/data/dto/ResponseLoginDTO.kt
+++ app/src/main/java/com/wevx/dealershipmanagement/data/dto/ResponseLoginDTO.kt
@@ -25,7 +25,6 @@ data class ResponseLoginDTO(
         @SerializedName("user")
         val user: User? = null
     ) {
-
         @Keep
         data class User(
             @SerializedName("avatar")
