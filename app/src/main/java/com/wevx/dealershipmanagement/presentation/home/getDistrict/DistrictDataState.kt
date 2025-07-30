ada2769ddb32eea2304308039316f8a75e6cc302
Rifat Hossain <mdrifathossain472@gmail.com>
2025-07-30 16:45:03 +0600

    initial commit

---
+++ app/src/main/java/com/wevx/dealershipmanagement/presentation/home/getDistrict/DistrictDataState.kt
@@ -0,0 +1,9 @@
+package com.wevx.dealershipmanagement.presentation.home.getDistrict
+
+import com.wevx.dealershipmanagement.domain.models.DistrictModel
+
+data class DistrictDataState(
+    val loading: Boolean = false,
+    val error: String? = null,
+    val data: List<DistrictModel>? = emptyList()
+)
