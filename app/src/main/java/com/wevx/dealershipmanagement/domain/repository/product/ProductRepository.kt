e01f40fc80de3432b3f4430ca7380c72af069026
Rifat Hossain <mdrifathossain472@gmail.com>
2025-07-31 03:04:57 +0600

    fetch all product api, category api, district api, sub district api, area api, update NetworkModule.kt, add services

--- app/src/main/java/com/wevx/dealershipmanagement/domain/repository/product/ProductRepository.kt
+++ app/src/main/java/com/wevx/dealershipmanagement/domain/repository/product/ProductRepository.kt
@@ -1,11 +1,14 @@
 package com.wevx.dealershipmanagement.domain.repository.product
 
-import com.wevx.dealershipmanagement.data.dto.responseCategoryDTO.ResponseCategoryDTO
+import com.wevx.dealershipmanagement.data.dto.categoryDTO.ResponseCategoryDTO
+import com.wevx.dealershipmanagement.data.dto.productDto.ResponseProductDTO
 import retrofit2.Response
 
 interface ProductRepository {
 
     suspend fun getCategory(): Response<ResponseCategoryDTO>
+
+    suspend fun getAllProduct(): Response<ResponseProductDTO>
 
 
 }
