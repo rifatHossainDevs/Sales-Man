af3cdb3cd80b1eeb25dedee829d3cd9df381e385
Rifat Hossain <mdrifathossain472@gmail.com>
2025-07-31 03:04:12 +0600

    fetch all product api, category api, district api, sub district api, area api, update NetworkModule.kt, add services

--- app/src/main/java/com/wevx/dealershipmanagement/domain/use_case/home_usecase/GetAreaUseCase.kt
+++ app/src/main/java/com/wevx/dealershipmanagement/domain/use_case/home_usecase/GetAreaUseCase.kt
@@ -1,21 +1,21 @@
 package com.wevx.dealershipmanagement.domain.use_case.home_usecase
 
 import com.wevx.dealershipmanagement.core.common.Resource
-import com.wevx.dealershipmanagement.data.dto.responseAreaDTO.toAreaModelList
+import com.wevx.dealershipmanagement.data.dto.areaDTO.toAreaModelList
 import com.wevx.dealershipmanagement.domain.models.AreaModel
-import com.wevx.dealershipmanagement.domain.repository.home.HomeRepository
+import com.wevx.dealershipmanagement.domain.repository.home.AreaRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class GetAreaUseCase @Inject constructor(
-    private val homeRepository: HomeRepository
+    private val areaRepository: AreaRepository
 ) {
     operator fun invoke(subDisId: Int): Flow<Resource<List<AreaModel>>> = flow {
         try {
             emit(Resource.Loading())
 
-            val response = homeRepository.getArea(subDisId)
+            val response = areaRepository.getArea(subDisId)
 
             if (response.isSuccessful) {
                 val body = response.body()
