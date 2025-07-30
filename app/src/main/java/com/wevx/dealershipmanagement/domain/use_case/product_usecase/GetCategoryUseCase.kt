0a309f63a80240f94fd7d5ebd9b7ef827f3d7259
Rifat Hossain <mdrifathossain472@gmail.com>
2025-07-31 03:04:18 +0600

    fetch all product api, category api, district api, sub district api, area api, update NetworkModule.kt, add services

--- app/src/main/java/com/wevx/dealershipmanagement/domain/use_case/product_usecase/GetCategoryUseCase.kt
+++ app/src/main/java/com/wevx/dealershipmanagement/domain/use_case/product_usecase/GetCategoryUseCase.kt
@@ -1,7 +1,7 @@
 package com.wevx.dealershipmanagement.domain.use_case.product_usecase
 
 import com.wevx.dealershipmanagement.core.common.Resource
-import com.wevx.dealershipmanagement.data.dto.responseCategoryDTO.toCategoryList
+import com.wevx.dealershipmanagement.data.dto.categoryDTO.toCategoryList
 import com.wevx.dealershipmanagement.domain.models.CategoryModel
 import com.wevx.dealershipmanagement.domain.repository.product.ProductRepository
 import kotlinx.coroutines.flow.Flow
