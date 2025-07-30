9d03e53b5658016d683670ee75ef4ea9ebe39512
Rifat Hossain <mdrifathossain472@gmail.com>
2025-07-31 03:02:16 +0600

    initial commit

--- app/src/main/java/com/wevx/dealershipmanagement/data/remote/home/AreaApiService.kt
+++ app/src/main/java/com/wevx/dealershipmanagement/data/remote/home/AreaApiService.kt
@@ -1,13 +1,13 @@
 package com.wevx.dealershipmanagement.data.remote.home
 
-import com.wevx.dealershipmanagement.data.dto.responseAreaDTO.ResponseAreaDTO
+import com.wevx.dealershipmanagement.data.dto.areaDTO.ResponseAreaDTO
 import com.wevx.dealershipmanagement.data.dto.districtDto.ResponseDisDTO
 import com.wevx.dealershipmanagement.data.dto.subDistrictDto.ResponseSubDisDTO
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 
-interface HomeApiService {
+interface AreaApiService {
 
     @GET("districts/get-district-by-div/{divId}")
     suspend fun getDistrict(@Path("divId") divId: Int): Response<ResponseDisDTO>
