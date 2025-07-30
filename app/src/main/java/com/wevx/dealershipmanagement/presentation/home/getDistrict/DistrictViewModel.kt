61131ca5d28cc47270a33745b66618693ac7acff
Rifat Hossain <mdrifathossain472@gmail.com>
2025-07-30 16:45:15 +0600

    initial commit

---
+++ app/src/main/java/com/wevx/dealershipmanagement/presentation/home/getDistrict/DistrictViewModel.kt
@@ -0,0 +1,51 @@
+package com.wevx.dealershipmanagement.presentation.home.getDistrict
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.wevx.dealershipmanagement.core.common.Resource
+import com.wevx.dealershipmanagement.data.dto.loginDto.RequestLogin
+import com.wevx.dealershipmanagement.domain.use_case.auth_usecase.LoginUseCase
+import com.wevx.dealershipmanagement.domain.use_case.home_usecase.GetDistrictUseCase
+import com.wevx.dealershipmanagement.presentation.auth.login.LoginDataState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class DistrictViewModel @Inject constructor(
+    private val districtUseCase: GetDistrictUseCase
+) : ViewModel() {
+
+    private val _districtState = MutableStateFlow(DistrictDataState())
+    val districtState: StateFlow<DistrictDataState> = _districtState
+
+
+    fun getDistrict(divId: Int) {
+        viewModelScope.launch {
+            _districtState.value = DistrictDataState(loading = true)
+            districtUseCase.invoke(divId).collect { response ->
+                when (response) {
+                    is Resource.Success -> {
+                        _districtState.value = DistrictDataState(data = response.data)
+                        Log.d("TAG", "district: ${response.data}")
+                    }
+
+                    is Resource.Loading -> {
+                        _districtState.value = DistrictDataState(loading = true)
+                    }
+
+                    is Resource.Error -> {
+                        _districtState.value = DistrictDataState(error = response.message)
+                        Log.d("TAG", "error: ${response.message}")
+                    }
+                }
+
+            }
+        }
+    }
+
+
+}
