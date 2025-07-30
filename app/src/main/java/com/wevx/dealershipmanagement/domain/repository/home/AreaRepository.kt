8fff7e7cf266550078edb67722ab8869f4d89fbe
Rifat Hossain <mdrifathossain472@gmail.com>
2025-07-31 03:02:22 +0600

    initial commit

--- app/src/main/java/com/wevx/dealershipmanagement/domain/repository/home/AreaRepository.kt
+++ app/src/main/java/com/wevx/dealershipmanagement/domain/repository/home/AreaRepository.kt
@@ -1,11 +1,11 @@
 package com.wevx.dealershipmanagement.domain.repository.home
 
-import com.wevx.dealershipmanagement.data.dto.responseAreaDTO.ResponseAreaDTO
+import com.wevx.dealershipmanagement.data.dto.areaDTO.ResponseAreaDTO
 import com.wevx.dealershipmanagement.data.dto.districtDto.ResponseDisDTO
 import com.wevx.dealershipmanagement.data.dto.subDistrictDto.ResponseSubDisDTO
 import retrofit2.Response
 
-interface HomeRepository {
+interface AreaRepository {
 
     suspend fun getDistrict(divId: Int): Response<ResponseDisDTO>
     suspend fun getSubDistrict(disId: Int): Response<ResponseSubDisDTO>
