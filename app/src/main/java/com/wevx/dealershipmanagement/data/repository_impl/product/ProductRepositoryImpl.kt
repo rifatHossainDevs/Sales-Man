26b3804bf942d3cd9e9f142da8c8643df3814f04
Rifat Hossain <mdrifathossain472@gmail.com>
2025-07-31 03:05:03 +0600

    fetch all product api, category api, district api, sub district api, area api, update NetworkModule.kt, add services

--- app/src/main/java/com/wevx/dealershipmanagement/data/repository_impl/product/ProductRepositoryImpl.kt
+++ app/src/main/java/com/wevx/dealershipmanagement/data/repository_impl/product/ProductRepositoryImpl.kt
@@ -1,12 +1,8 @@
 package com.wevx.dealershipmanagement.data.repository_impl.product
 
-import com.wevx.dealershipmanagement.data.dto.responseAreaDTO.ResponseAreaDTO
-import com.wevx.dealershipmanagement.data.dto.districtDto.ResponseDisDTO
-import com.wevx.dealershipmanagement.data.dto.responseCategoryDTO.ResponseCategoryDTO
-import com.wevx.dealershipmanagement.data.dto.subDistrictDto.ResponseSubDisDTO
-import com.wevx.dealershipmanagement.data.remote.home.HomeApiService
+import com.wevx.dealershipmanagement.data.dto.categoryDTO.ResponseCategoryDTO
+import com.wevx.dealershipmanagement.data.dto.productDto.ResponseProductDTO
 import com.wevx.dealershipmanagement.data.remote.product.ProductApiService
-import com.wevx.dealershipmanagement.domain.repository.home.HomeRepository
 import com.wevx.dealershipmanagement.domain.repository.product.ProductRepository
 import jakarta.inject.Inject
 import retrofit2.Response
@@ -18,6 +14,10 @@ class ProductRepositoryImpl @Inject constructor(
     override suspend fun getCategory(): Response<ResponseCategoryDTO> {
         return productApiService.getCategory()
 
+    }
+
+    override suspend fun getAllProduct(): Response<ResponseProductDTO> {
+        return productApiService.getAllProduct()
     }
 
 
