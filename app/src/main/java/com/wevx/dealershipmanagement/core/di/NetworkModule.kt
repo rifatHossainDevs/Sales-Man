4109c83123f5c603dc6b4a7a555d400a7f973c5d
Rifat Hossain <mdrifathossain472@gmail.com>
2025-07-30 16:47:50 +0600

    initial commit

--- app/src/main/java/com/wevx/dealershipmanagement/core/di/NetworkModule.kt
+++ app/src/main/java/com/wevx/dealershipmanagement/core/di/NetworkModule.kt
@@ -1,8 +1,12 @@
 package com.wevx.dealershipmanagement.core.di
 
-import com.wevx.dealershipmanagement.data.remote.AuthApiService
-import com.wevx.dealershipmanagement.data.repository_impl.AuthRepositoryImpl
-import com.wevx.dealershipmanagement.domain.repository.AuthRepository
+import com.wevx.dealershipmanagement.data.remote.auth.AuthApiService
+import com.wevx.dealershipmanagement.data.remote.home.HomeApiService
+import com.wevx.dealershipmanagement.data.repository_impl.auth.AuthRepositoryImpl
+import com.wevx.dealershipmanagement.data.repository_impl.home.HomeRepositoryImpl
+import com.wevx.dealershipmanagement.domain.repository.auth.AuthRepository
+import com.wevx.dealershipmanagement.domain.repository.home.HomeRepository
+import com.wevx.dealershipmanagement.domain.use_case.home_usecase.GetDistrictUseCase
 import com.wevx.dealershipmanagement.utils.Constants
 import dagger.Module
 import dagger.Provides
@@ -19,7 +23,7 @@ object NetworkModule {
 
     @Provides
     @Singleton
-    fun provideRetrofit(): Retrofit{
+    fun provideRetrofit(): Retrofit {
         return Retrofit.Builder()
             .baseUrl(Constants.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
@@ -37,6 +41,27 @@ object NetworkModule {
     @Singleton
     fun provideAuthRepository(authApiService: AuthApiService): AuthRepository {
         return AuthRepositoryImpl(authApiService)
+    }
+
+    //Home
+    @Provides
+    @Singleton
+    fun provideHomeApiService(retrofit: Retrofit): HomeApiService {
+        return retrofit.create(HomeApiService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideHomeRepository(homeApiService: HomeApiService): HomeRepository {
+        return HomeRepositoryImpl(homeApiService)
+    }
+
+    @Provides
+    @Singleton
+    fun provideDistrictUseCase(
+        homeRepository: HomeRepository
+    ): GetDistrictUseCase {
+        return GetDistrictUseCase(homeRepository)
     }
 
     // Profile
