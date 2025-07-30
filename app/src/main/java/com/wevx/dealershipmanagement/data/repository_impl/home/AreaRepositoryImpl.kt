5ea7ebebdd4cc4cf343f54c095ce826facf6e8f2
Rifat Hossain <mdrifathossain472@gmail.com>
2025-07-31 03:02:31 +0600

    initial commit

--- app/src/main/java/com/wevx/dealershipmanagement/data/repository_impl/home/AreaRepositoryImpl.kt
+++ app/src/main/java/com/wevx/dealershipmanagement/data/repository_impl/home/AreaRepositoryImpl.kt
@@ -1,28 +1,28 @@
 package com.wevx.dealershipmanagement.data.repository_impl.home
 
-import com.wevx.dealershipmanagement.data.dto.responseAreaDTO.ResponseAreaDTO
+import com.wevx.dealershipmanagement.data.dto.areaDTO.ResponseAreaDTO
 import com.wevx.dealershipmanagement.data.dto.districtDto.ResponseDisDTO
 import com.wevx.dealershipmanagement.data.dto.subDistrictDto.ResponseSubDisDTO
-import com.wevx.dealershipmanagement.data.remote.home.HomeApiService
-import com.wevx.dealershipmanagement.domain.repository.home.HomeRepository
+import com.wevx.dealershipmanagement.data.remote.home.AreaApiService
+import com.wevx.dealershipmanagement.domain.repository.home.AreaRepository
 import jakarta.inject.Inject
 import retrofit2.Response
 
-class HomeRepositoryImpl @Inject constructor(
-    private val homeApiService: HomeApiService
-): HomeRepository {
+class AreaRepositoryImpl @Inject constructor(
+    private val areaApiService: AreaApiService
+): AreaRepository {
 
     override suspend fun getDistrict(divId: Int): Response<ResponseDisDTO> {
-        return homeApiService.getDistrict(divId)
+        return areaApiService.getDistrict(divId)
 
     }
 
     override suspend fun getSubDistrict(disId: Int): Response<ResponseSubDisDTO> {
-        return homeApiService.getSubDistrict(disId)
+        return areaApiService.getSubDistrict(disId)
     }
 
     override suspend fun getArea(subDisId: Int): Response<ResponseAreaDTO> {
-        return homeApiService.getArea(subDisId)
+        return areaApiService.getArea(subDisId)
     }
 
 
