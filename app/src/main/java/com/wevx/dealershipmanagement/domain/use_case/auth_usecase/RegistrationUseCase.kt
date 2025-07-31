450b91dbe5c7a118487de77b0d03d24710a9182b
Rifat Hossain <mdrifathossain472@gmail.com>
2025-08-01 03:52:17 +0600

    fetch all product api, category api, district api, sub district api, area api, update NetworkModule.kt, add services

---
+++ app/src/main/java/com/wevx/dealershipmanagement/domain/use_case/auth_usecase/RegistrationUseCase.kt
@@ -0,0 +1,34 @@
+package com.wevx.dealershipmanagement.domain.use_case.auth_usecase
+
+import com.wevx.dealershipmanagement.core.common.Resource
+import com.wevx.dealershipmanagement.data.dto.registrationDto.RequestRegistration
+import com.wevx.dealershipmanagement.data.dto.registrationDto.toRegistrationModel
+import com.wevx.dealershipmanagement.domain.models.RegistrationModel
+import com.wevx.dealershipmanagement.domain.repository.auth.AuthRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class RegistrationUseCase @Inject constructor(
+    private val authRepository: AuthRepository
+) {
+    operator fun invoke(requestRegistration: RequestRegistration): Flow<Resource<RegistrationModel>> = flow {
+        try {
+            emit(Resource.Loading())
+            val response = authRepository.registration(requestRegistration)
+
+            if (response.isSuccessful) {
+                val data = response.body()?.data?.toRegistrationModel()
+                if (data != null) {
+                    emit(Resource.Success(data))
+                } else {
+                    emit(Resource.Error("Registration failed: No data received"))
+                }
+            } else {
+                emit(Resource.Error("Registration failed: ${response.message()}"))
+            }
+        } catch (e: Exception) {
+            emit(Resource.Error("An error occurred: ${e.localizedMessage}"))
+        }
+    }
+}
