cf02c1d340d235af4f2502a367a13521e424d73c
Rifat Hossain <mdrifathossain472@gmail.com>
2025-08-01 03:52:57 +0600

    fetch all product api, category api, district api, sub district api, area api, update NetworkModule.kt, add services

---
+++ app/src/main/java/com/wevx/dealershipmanagement/data/dto/registrationDto/ResponseRegistrationDTO.kt
@@ -0,0 +1,53 @@
+package com.wevx.dealershipmanagement.data.dto.registrationDto
+
+import kotlinx.serialization.Serializable
+import androidx.annotation.Keep
+import com.google.gson.annotations.SerializedName
+import com.wevx.dealershipmanagement.domain.models.RegistrationModel
+
+@Keep
+@Serializable
+data class ResponseRegistrationDTO(
+    @SerializedName("data")
+    val `data`: Data? = null,
+    @SerializedName("message")
+    val message: String? = null,
+    @SerializedName("statusCode")
+    val statusCode: Int? = null,
+    @SerializedName("success")
+    val success: Boolean? = null
+) {
+    @Keep
+    @Serializable
+    data class Data(
+        @SerializedName("avatar")
+        val avatar: String? = null,
+        @SerializedName("createdAt")
+        val createdAt: String? = null,
+        @SerializedName("email")
+        val email: String? = null,
+        @SerializedName("fullName")
+        val fullName: String? = null,
+        @SerializedName("_id")
+        val id: String? = null,
+        @SerializedName("phone")
+        val phone: String? = null,
+        @SerializedName("updatedAt")
+        val updatedAt: String? = null,
+        @SerializedName("userType")
+        val userType: String? = null,
+        @SerializedName("__v")
+        val v: Int? = null
+    )
+}
+
+fun ResponseRegistrationDTO.Data.toRegistrationModel(): RegistrationModel {
+    return RegistrationModel(
+        userId = this.id ?: "",
+        userEmail = this.email ?: "",
+        userName = this.fullName ?: "",
+        userPhoneNumber = this.phone ?: "",
+        userProfileImageUrl = this.avatar ?: "",
+        userRole = this.userType ?: ""
+    )
+}
