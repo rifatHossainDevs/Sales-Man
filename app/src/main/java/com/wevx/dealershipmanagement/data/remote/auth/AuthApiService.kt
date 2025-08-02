ad8ebdb037ddb844c94c87a96429311c8ebc4b15
Rifat Hossain <mdrifathossain472@gmail.com>
2025-08-03 02:29:40 +0600

    update AuthApiService.kt

--- app/src/main/java/com/wevx/dealershipmanagement/data/remote/auth/AuthApiService.kt
+++ app/src/main/java/com/wevx/dealershipmanagement/data/remote/auth/AuthApiService.kt
@@ -1,11 +1,14 @@
 package com.wevx.dealershipmanagement.data.remote.auth
 
+import com.wevx.dealershipmanagement.data.dto.changePasswordDTO.RequestChangePasswordDto
+import com.wevx.dealershipmanagement.data.dto.changePasswordDTO.ResponseChangePasswordDTO
 import com.wevx.dealershipmanagement.data.dto.loginDto.RequestLogin
 import com.wevx.dealershipmanagement.data.dto.loginDto.ResponseLoginDTO
-import com.wevx.dealershipmanagement.data.dto.registrationDto.RequestRegistration
+import com.wevx.dealershipmanagement.data.dto.profileDTO.ResponseProfileDto
 import com.wevx.dealershipmanagement.data.dto.registrationDto.ResponseRegistrationDTO
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface AuthApiService {
@@ -16,7 +19,11 @@ interface AuthApiService {
     @POST("users/register")
     suspend fun registration(@Body requestRegistration: RequestRegistration): Response<ResponseRegistrationDTO>
 
+    @GET("users/current-user")
+    suspend fun getProfile(): Response<ResponseProfileDto>
 
+    @POST("users/change-password")
+    suspend fun changePassword(@Body requestChangePassword: RequestChangePasswordDto): Response<ResponseChangePasswordDTO>
 
 
 }
