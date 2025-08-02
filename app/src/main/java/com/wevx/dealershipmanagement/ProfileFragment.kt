f2f98e76c0d9bc65a3524c5bd371474d2d84a4f6
Rifat Hossain <mdrifathossain472@gmail.com>
2025-08-03 02:35:15 +0600

    add ProfileFragment.kt

--- app/src/main/java/com/wevx/dealershipmanagement/ProfileFragment.kt
+++ app/src/main/java/com/wevx/dealershipmanagement/ProfileFragment.kt
@@ -1,59 +1,42 @@
 package com.wevx.dealershipmanagement
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import coil.load
+import com.wevx.dealershipmanagement.core.common.BaseFragment
+import com.wevx.dealershipmanagement.databinding.FragmentProfileBinding
+import com.wevx.dealershipmanagement.presentation.auth.profile.ProfileViewModel
+import com.wevx.dealershipmanagement.utils.collectInLifecycle
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class ProfileFragment : BaseFragment<FragmentProfileBinding>(FragmentProfileBinding::inflate) {
+    private val viewModel: ProfileViewModel by viewModels()
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun setAllClickListener() {
+        viewModel.getProfile()
+        binding.btnEditProfile.setOnClickListener {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
-    }
+    override fun allObserver() {
+        viewModel.profileState.collectInLifecycle(viewLifecycleOwner) { profileState ->
+            if (profileState.loading) return@collectInLifecycle
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+            profileState.error?.let { error->
+                Toast.makeText(requireContext(), "Error: $error", Toast.LENGTH_SHORT).show()
+            }
+
+            profileState.data?.let {data->
+                Toast.makeText(requireContext(), "Success : $data", Toast.LENGTH_SHORT).show()
+                binding.apply {
+                    tvName.text = data.name
+                    tvEmail.text = data.email
+                    tvPhone.text = data.phone
+                    tvUserType.text = data.userType
+                    tvNid.text = data.nid
+                    ivProfileImage.load(data.avatar)
                 }
             }
+        }
     }
+
 }
