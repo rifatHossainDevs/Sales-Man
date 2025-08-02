76788a54d9f1692bd7397a2379eab6fa351540b5
Rifat Hossain <mdrifathossain472@gmail.com>
2025-08-03 00:56:07 +0600

    update LoginFragment.kt

---
+++ app/src/main/java/com/wevx/dealershipmanagement/domain/models/ProfileModel.kt
@@ -0,0 +1,11 @@
+package com.wevx.dealershipmanagement.domain.models
+
+data class ProfileModel(
+    val id: String,
+    val name: String,
+    val email: String,
+    val phone: String,
+    val nid: String,
+    val avatar: String,
+    val userType: String
+)
