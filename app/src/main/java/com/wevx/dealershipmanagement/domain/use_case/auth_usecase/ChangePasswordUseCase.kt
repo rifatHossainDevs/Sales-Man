8106a23b354f094a83162741e83b7a7a4c5205eb
Rifat Hossain <mdrifathossain472@gmail.com>
2025-08-03 02:31:19 +0600

    add ChangePassword Fragment, fetch ChangePassword api

---
+++ app/src/main/java/com/wevx/dealershipmanagement/domain/use_case/auth_usecase/ChangePasswordUseCase.kt
@@ -0,0 +1,33 @@
+package com.wevx.dealershipmanagement.domain.use_case.auth_usecase
+
+import com.wevx.dealershipmanagement.core.common.Resource
+import com.wevx.dealershipmanagement.data.dto.changePasswordDTO.RequestChangePasswordDto
+import com.wevx.dealershipmanagement.data.dto.changePasswordDTO.ResponseChangePasswordDTO
+import com.wevx.dealershipmanagement.domain.repository.auth.AuthRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class ChangePasswordUseCase @Inject constructor(
+    private val authRepository: AuthRepository
+) {
+    operator fun invoke(requestChangePassword: RequestChangePasswordDto): Flow<Resource<ResponseChangePasswordDTO>> = flow {
+        try {
+            emit(Resource.Loading())
+            val response = authRepository.changePassword(requestChangePassword)
+
+            if (response.isSuccessful) {
+                val data = response.body()
+                if (data != null) {
+                    emit(Resource.Success(data))
+                } else {
+                    emit(Resource.Error("Change Password failed"))
+                }
+            } else {
+                emit(Resource.Error("Change Password failed: ${response.message()}"))
+            }
+        } catch (e: Exception) {
+            emit(Resource.Error("An error occurred: ${e.localizedMessage}"))
+        }
+    }
+}
