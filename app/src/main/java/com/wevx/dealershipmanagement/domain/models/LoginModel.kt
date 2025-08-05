f01e1a8467e24080ae49c7b47a8553a62394a674
Rifat Hossain <mdrifathossain472@gmail.com>
2025-08-06 03:33:49 +0600

    update login, registration, add StoreOwnerDetailsFragment.kt

--- app/src/main/java/com/wevx/dealershipmanagement/domain/models/LoginModel.kt
+++ app/src/main/java/com/wevx/dealershipmanagement/domain/models/LoginModel.kt
@@ -2,12 +2,14 @@ package com.wevx.dealershipmanagement.domain.models
 
 data class LoginModel(
     val userId: String,
-    val userName: String,
     val userEmail: String,
+    val userName: String,
     val userPhoneNumber: String,
-    val userRole: String,
+    val nidNumber: String? = null,
     val userProfileImageUrl: String? = null,
-    val nid: String? = null,
+    val userType: String,
+    val companyId: String? = null,
+    val isActive: Boolean,
     val refreshToken: String? = null,
     val accessToken: String? = null
 )
