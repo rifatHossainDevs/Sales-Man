78c52dd68a07b2399406bc9326e6f1a556259760
Rifat Hossain <mdrifathossain472@gmail.com>
2025-08-06 03:32:56 +0600

    update login, registration, add StoreOwnerDetailsFragment.kt

--- app/src/main/java/com/wevx/dealershipmanagement/presentation/auth/profile/EditProfileViewModel.kt
+++ app/src/main/java/com/wevx/dealershipmanagement/presentation/auth/profile/EditProfileViewModel.kt
@@ -4,7 +4,6 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.wevx.dealershipmanagement.core.common.Resource
-import com.wevx.dealershipmanagement.data.dto.loginDto.RequestLogin
 import com.wevx.dealershipmanagement.domain.use_case.auth_usecase.GetProfileUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -13,28 +12,28 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class ProfileViewModel @Inject constructor(
+class EditProfileViewModel @Inject constructor(
     private val profileUseCase: GetProfileUseCase
 ) : ViewModel() {
-    private val _profileState = MutableStateFlow(ProfileDataState())
-    val profileState: StateFlow<ProfileDataState> = _profileState
+    private val _profileState = MutableStateFlow(EditProfileDataState())
+    val profileState: StateFlow<EditProfileDataState> = _profileState
 
     fun getProfile() {
         viewModelScope.launch {
-            _profileState.value = ProfileDataState(loading = true)
+            _profileState.value = EditProfileDataState(loading = true)
             profileUseCase.invoke().collect { response ->
                 when (response) {
                     is Resource.Success -> {
-                        _profileState.value = ProfileDataState(data = response.data)
+                        _profileState.value = EditProfileDataState(data = response.data)
                         Log.d("profileData", "getProfile: ${response.data}")
                     }
 
                     is Resource.Loading -> {
-                        _profileState.value = ProfileDataState(loading = true)
+                        _profileState.value = EditProfileDataState(loading = true)
                     }
 
                     is Resource.Error -> {
-                        _profileState.value = ProfileDataState(error = response.message)
+                        _profileState.value = EditProfileDataState(error = response.message)
                     }
                 }
 
