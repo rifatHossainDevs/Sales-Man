f8c7addc486e72a60cb45372f080a41ace56a84d
Rifat Hossain <mdrifathossain472@gmail.com>
2025-08-06 18:52:54 +0600

    update login, registration, add StoreOwnerDetailsFragment.kt

--- app/src/main/java/com/wevx/dealershipmanagement/presentation/storeOwnerDetails/StoreOwnerDetailsFragment.kt
+++ app/src/main/java/com/wevx/dealershipmanagement/presentation/storeOwnerDetails/StoreOwnerDetailsFragment.kt
@@ -1,10 +1,12 @@
-package com.wevx.dealershipmanagement
+package com.wevx.dealershipmanagement.presentation.storeOwnerDetails
 
 import androidx.navigation.fragment.findNavController
+import com.wevx.dealershipmanagement.R
 import com.wevx.dealershipmanagement.core.common.BaseFragment
 import com.wevx.dealershipmanagement.databinding.FragmentStoreOwnerDetailsBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class StoreOwnerDetailsFragment : BaseFragment<FragmentStoreOwnerDetailsBinding>(
     FragmentStoreOwnerDetailsBinding::inflate
 ) {
