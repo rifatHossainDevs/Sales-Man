f142e0e8c1c3dc40c66f5bed7827fea22019c87f
Rifat Hossain <mdrifathossain472@gmail.com>
2025-08-08 03:44:04 +0600

    update login, registration, add StoreOwnerDetailsFragment.kt

---
+++ app/src/main/java/com/wevx/dealershipmanagement/domain/use_case/order/GetPendingOrderUseCase.kt
@@ -0,0 +1,38 @@
+package com.wevx.dealershipmanagement.domain.use_case.order
+
+import com.wevx.dealershipmanagement.core.common.Resource
+import com.wevx.dealershipmanagement.data.dto.pendingOrderDto.toPendingOrderModelList
+import com.wevx.dealershipmanagement.data.dto.productDto.toProductModelList
+import com.wevx.dealershipmanagement.domain.models.PendingOrderModel
+import com.wevx.dealershipmanagement.domain.models.ProductModel
+import com.wevx.dealershipmanagement.domain.repository.order.OrderRepository
+import com.wevx.dealershipmanagement.domain.repository.product.ProductRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class GetPendingOrderUseCase @Inject constructor(
+    private val orderRepository: OrderRepository
+) {
+    operator fun invoke(customerId: String): Flow<Resource<List<PendingOrderModel>>> = flow {
+        try {
+            emit(Resource.Loading())
+
+            val response = orderRepository.getPendingOrdersByCustomer(customerId)
+
+            if (response.isSuccessful) {
+                val body = response.body()
+                val responseList = body?.data ?: emptyList()
+                val data = responseList.toPendingOrderModelList()
+
+                emit(Resource.Success(data = data))
+            } else {
+                emit(Resource.Error("Pending order fetch failed: ${response.message()}"))
+            }
+
+        } catch (e: Exception) {
+            emit(Resource.Error("An error occurred: ${e.localizedMessage ?: "Unknown error"}"))
+        }
+    }
+
+}
