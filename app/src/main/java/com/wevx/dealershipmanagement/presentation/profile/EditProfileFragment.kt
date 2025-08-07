1e38dbd6197bfc60b3c321ddd56ef94c9c7ecf5d
Rifat Hossain <mdrifathossain472@gmail.com>
2025-08-08 03:43:13 +0600

    update login, registration, add StoreOwnerDetailsFragment.kt

---
+++ app/src/main/java/com/wevx/dealershipmanagement/presentation/profile/EditProfileFragment.kt
@@ -0,0 +1,17 @@
+package com.wevx.dealershipmanagement.presentation.profile
+
+import com.wevx.dealershipmanagement.core.common.BaseFragment
+import com.wevx.dealershipmanagement.databinding.FragmentEditProfileBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class EditProfileFragment : BaseFragment<FragmentEditProfileBinding>(FragmentEditProfileBinding::inflate) {
+    override fun setAllClickListener() {
+
+    }
+
+    override fun allObserver() {
+
+    }
+
+}
