6c26dc8e3518bf4aa68986a76cff6ddfb3caf26c
Rifat Hossain <mdrifathossain472@gmail.com>
2025-08-11 00:54:08 +0600

    add edit profile, update logout, add auto login

---
+++ app/src/main/java/com/wevx/dealershipmanagement/data/dto/updateProfileDto/RequestUpdateProfile.kt
@@ -0,0 +1,18 @@
+package com.wevx.dealershipmanagement.data.dto.updateProfileDto
+
+import kotlinx.serialization.Serializable
+import androidx.annotation.Keep
+import com.google.gson.annotations.SerializedName
+
+@Keep
+@Serializable
+data class RequestUpdateProfile(
+    @SerializedName("email")
+    val email: String? = null,
+    @SerializedName("fullName")
+    val fullName: String? = null,
+    @SerializedName("nidNumber")
+    val nidNumber: String? = null,
+    @SerializedName("phone")
+    val phone: String? = null
+)
