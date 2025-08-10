20ec5c0d3733195e58c288809cd080efb6ab0342
Rifat Hossain <mdrifathossain472@gmail.com>
2025-08-11 03:32:57 +0600

    add edit profile, update logout, add auto login

---
+++ app/src/main/java/com/wevx/dealershipmanagement/domain/use_case/storeOwner/GetStoreOwnerByIdUseCase.kt
@@ -0,0 +1,34 @@
+package com.wevx.dealershipmanagement.domain.use_case.storeOwner
+
+import com.wevx.dealershipmanagement.core.common.Resource
+import com.wevx.dealershipmanagement.data.dto.getStoreById.toStoreOwnerById
+import com.wevx.dealershipmanagement.domain.models.StoreOwnerByIdModel
+import com.wevx.dealershipmanagement.domain.repository.storeOwner.StoreOwnerRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class GetStoreOwnerByIdUseCase @Inject constructor(
+    private val storeOwnerRepository: StoreOwnerRepository
+) {
+    operator fun invoke(id: String): Flow<Resource<StoreOwnerByIdModel>> = flow {
+        try {
+            emit(Resource.Loading())
+
+            val response = storeOwnerRepository.getStoreOwnerId(id)
+
+            if (response.isSuccessful) {
+                val storeOwner = response.body()?.data?.toStoreOwnerById()
+                if (storeOwner != null) {
+                    emit(Resource.Success(storeOwner))
+                } else {
+                    emit(Resource.Error("No store owner found for the given ID"))
+                }
+            } else {
+                emit(Resource.Error("Store Owner fetch failed: ${response.message()}"))
+            }
+        } catch (e: Exception) {
+            emit(Resource.Error("An error occurred: ${e.localizedMessage ?: "Unknown error"}"))
+        }
+    }
+}
