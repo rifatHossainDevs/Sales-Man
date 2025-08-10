f0889310f8380fa199951559eed3e4c9ded6dc7e
Rifat Hossain <mdrifathossain472@gmail.com>
2025-08-11 00:50:49 +0600

    update login, registration, add StoreOwnerDetailsFragment.kt

--- app/src/main/java/com/wevx/dealershipmanagement/data/remote/auth/AuthApiService.kt
+++ app/src/main/java/com/wevx/dealershipmanagement/data/remote/auth/AuthApiService.kt
@@ -8,10 +8,13 @@ import com.wevx.dealershipmanagement.data.dto.logoutDTO.ResponseLogoutDto
 import com.wevx.dealershipmanagement.data.dto.profileDTO.ResponseProfileDto
 import com.wevx.dealershipmanagement.data.dto.registrationDto.RequestRegistrationDto
 import com.wevx.dealershipmanagement.data.dto.registrationDto.ResponseRegistrationDTO
+import com.wevx.dealershipmanagement.data.dto.updateProfileDto.RequestUpdateProfile
+import com.wevx.dealershipmanagement.data.dto.updateProfileDto.ResponseUpdateProfileDto
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.PATCH
 import retrofit2.http.POST
 
 interface AuthApiService {
@@ -33,5 +36,11 @@ interface AuthApiService {
 
     @POST("users/logout")
     suspend fun logout(@Header("Authorization") token: String): Response<ResponseLogoutDto>
+
+    @PATCH("users/update-account")
+    suspend fun updateProfile(
+        @Header("Authorization") token: String,
+        @Body requestUpdateProfile: RequestUpdateProfile
+    ): Response<ResponseUpdateProfileDto>
 
 }
